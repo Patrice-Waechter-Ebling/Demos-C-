0aee8117addb76b4691033e1b2cc66a3fc2716eb
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 10:26:00 -0400

    Ajout de nouvelles demos

---
+++ CalculePaye/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ [21:38] CHANTAL DUSABIMANA INGABIRE (Etudiant)
+Exercice 1.   Un employeur désire un programme qui calculera la paie hebdomadaire 
+ * de ses employés payés à l’heure. 
+ * Le programme doit demander le nom de l’employé, son taux horaire et le nombre d’heures travaillées. 
+ * Par la suite, le programme doit afficher le nom de l’employé, le salaire brut, le salaire net, 
+ * le nombre d’heures travaillées et l’impôt retenu. 
+ * Le salaire brut est calculé ainsi : 40 premières heures au taux régulier, les heures supplémentaires à taux et demi. 
+ * L’impôt retenu est calculé de la façon suivante : si le salaire hebdomadaire multiplié par 52 est inférieur ou égal à 10 000 $
+ * , on retient 10 % du salaire brut. S’il est plus grand que 10 000 $, on lui retient 15 % d’impôt
+
+*/
+namespace CalculePaye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double impot = 0;
+            Console.Write("Saisissez votre nom\t"); string nom = Console.ReadLine();
+            Console.Write("Saisissez le nombre d'heures travailés\t"); double nHeures = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Saisissez votre tarif horraire\t"); double TarifHeure = Convert.ToDouble(Console.ReadLine());
+            double salaireHeb = TarifHeure * 40;
+            if (nHeures > 40)
+            {
+                salaireHeb = ((nHeures - 40) * 1.5) * TarifHeure + (nHeures * TarifHeure);
+            }
+            else {
+                salaireHeb = nHeures * TarifHeure;
+            }
+            double salaireAn = salaireHeb * 52;
+            if (salaireAn <= 10000)
+            {
+                impot = salaireAn * .1;
+            }
+            else {
+                impot = salaireAn * .15;
+            }
+            Console.WriteLine("Mr. \t"+nom);
+            Console.WriteLine("Heures travaillees \t"+nHeures.ToString());
+            Console.WriteLine("Salaire brut\t"+salaireAn.ToString());
+            Console.WriteLine("Salaire net\t"+Convert.ToString(salaireAn- impot));
+            Console.WriteLine("Retenue impot \t"+Convert.ToString(impot));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FormuleQuadratique/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FormuleQuadratique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.WriteLine("Projet d'application visant a utiliser la formule quadratique\nEntrez la  valeur de a (les x²)");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entrez la  valeur de a (les x)" );
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Entrez la  valeur de la constante");
+            c = Convert.ToDouble(Console.ReadLine());
+            d =( -b + Math.Sqrt((b * b) - 4*a*c)) / (2 * a);
+            Console.Write($"x1= {d}\n");
+            d = (-b-Math.Sqrt((b * b) - 4 * a * c)) /( 2 * a);
+            Console.Write($"x2={d}\n");
+        }
+    }
+}
