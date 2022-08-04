3720f01dd1ac0bf535b9a06122baa5ea07759300
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 10:14:42 -0400

    Add files via upload

---
+++ ASCII_TexteScanner/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ASCII_TexteScanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez une chaine de caracteres svp: ");
+            string tmp = Console.ReadLine();
+            ASCII_TexteScannerFunc(tmp);
+               
+        }
+        static void ASCII_TexteScannerFunc(string texte)
+        {
+            int[] alpha = { 0xFF };
+            int x = 0;
+            do
+            {
+                for(int y=0x13; y<0xFF; y++)
+                {
+                    if (texte[x] == y) alpha[x] = y;
+                    Console.Write($"{texte[x]} donne le caractere {(char)y} contenu {alpha[x]} fois dans la sequence");
+                }
+            } while( x < texte.Length);
+            for (int a=0; a <0xFF; a++)
+            {
+                Console.WriteLine($"{a} donne le caractere {(char)a} contenu {alpha[a]} fois dans la sequence");
+            }
+        }
+    }
+}
---
+++ AnalyseurTestextV2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AnalyseurTestextV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "AnalyseurTestextV2  Patrice Waechter-Ebling 14-02-2022";
+            AnalyserTexte(Console.Title);
+        }
+        static void AnalyserTexte(string texte)
+        {
+            string[] beta = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
+            string tmp = null;
+            int digit = 0;
+            int autre = 0;
+            char txt ;
+            int b = texte.Length;
+            Console.WriteLine($"Début de la procédure avec {tmp}");
+            for (int x = 0; x < b; x++)
+            {
+                if (char.IsLetter(texte[x]))
+                {
+                    for (int z = 97; z < 123; z++)
+                    {
+                        if (texte[x] == z)
+                        {
+                            int y = 0;
+                            for (int a = 0; a < b; a++)
+                            {
+                                if (char.IsWhiteSpace(texte[x]))
+                                {
+                                    Console.WriteLine($"Espace détecté offset: {x}");
+                                }
+                                if (texte.Substring(a, 1).Contains((char)z)) {
+                                    y++; 
+                                    txt = (char)y;
+                                        beta[2] = txt.ToString();
+                                    Console.WriteLine(beta[2]);
+                                }
+                            }
+                            tmp = tmp + (char)(z);
+                        }
+                    }
+                }
+                else if (char.IsDigit(texte[x]))
+                {
+                    digit++;
+                    tmp = tmp + (char)texte[x];
+                }
+                else
+                {
+                    autre++;
+                    tmp = tmp + texte[x];
+                }
+            }
+            for (int x = 0; x < 26; x++)
+            {
+                Console.WriteLine($"Nombre occurences de {(char)(x + 97)} ou {(char)(x + 65) } dans le texte = { beta[2] }.");
+            }
+            Console.WriteLine($"Nombre total de caractères numériques dans le texte = {digit}.");
+            Console.WriteLine($"Nombre total d'occurences de caractères de ponctuations parmi .,;!?  =  {autre}.");
+            Console.WriteLine(texte);
+        }
+    }
+}
