6dc68cae5ce159b5d0d5e39d62d9e6bf54c28953
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 10:31:34 -0400

    Ajout de nouvelles demos

---
+++ ListerFichiers/ListerFichiers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace ListerFichiers
+{
+    class ListerFichiers
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+             string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+             var dirnames = Directory.GetDirectories(Directory.GetCurrentDirectory());
+             Console.WriteLine(Directory.GetCurrentDirectory());
+            int i = 0;
+
+            try
+            {
+                foreach (var dir in dirnames)
+                {
+                    var fnames = Directory.GetFiles(dir);
+
+                    DirectoryInfo d = new DirectoryInfo(dir);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write(dir);
+                    FileInfo[] finfo = d.GetFiles("*.*");
+                    Console.WriteLine(" modification le : {0} ", File.GetLastWriteTime(dir));
+
+                    foreach (var f in fnames)
+                    {
+                        i++;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("Index "+ i);
+                        Console.Write(Path.Combine(dir, f));
+                            Console.WriteLine(" modification le : {0} ", File.GetLastWriteTime(dir));
+  /*                      foreach (FileInfo fi in finfo)
+                        {
+                        }**/
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
