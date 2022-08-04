103c36a8312e0960d3acea2f453dfc451b18bd7e
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 09:45:52 -0400

    Ajout de nouvelles demos

---
+++ Booleens/Program.cs
@@ -0,0 +1,23 @@
+/*********************************************
+		*
+* Projet genere avec CSProjectCreator v:1.01b
+		*
+*  Module:  Booleens
+* Developpe par Patrice Waechter-Ebling 2022
+		*********************************************/
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	namespace Booleens{	    internal class Program  {
+	        static void Main(string[] args){
+//Votre segment de code  a été inséré ici
+            bool isFishTasty = true;
+            bool isCSharpFun = false;
+            Console.WriteLine("Booleen: " + isCSharpFun);   // Outputs True
+            Console.WriteLine("Booleen: " + isFishTasty);   // Outputs False
+
+		 Console.WriteLine("Appuyez une touche pour quitter");Console.Read(); // informe l'utilisateur de presser une touche pour fermer la console
+		}
+    }
+}
