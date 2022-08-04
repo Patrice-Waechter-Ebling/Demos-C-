996bb357b52621a41ee8abc682bccf8f1cb5731f
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 12:41:01 -0400

    Ajout nouvelles demos

---
+++ Demo_Param2_ref/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Demo_Param2_ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            double b = 2;
+            decimal c = 2;
+            string d="Patate";
+
+            Console.WriteLine("---> Avant :");
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+            Console.WriteLine($"c : {c}");
+            Console.WriteLine($"d : {d}");
+            MethodeParamDemo(a,ref b,c,ref d);
+            Console.WriteLine("---> Après :");
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+            Console.WriteLine($"c : {c}");
+            Console.WriteLine($"d : {d}");
+        }
+        static void MethodeParamDemo(int a, ref double b, decimal c, ref string d)
+        {
+            a *= 2;
+            b *= 2;
+            c *= 2;
+            d = d + d;
+            Console.WriteLine("---> Dans la méthode (à la fin):");
+            Console.WriteLine($"a : {a}");
+            Console.WriteLine($"b : {b}");
+            Console.WriteLine($"c : {c}");
+            Console.WriteLine($"d : {d}");
+        }
+    }
+}
---
+++ Demo_Param_TryParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo_Param_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string EntreeDelUtilisateur;
+
+            EntreeDelUtilisateur = Console.ReadLine();
+
+            int ValeurConvertie;
+
+            if (Int32.TryParse(EntreeDelUtilisateur, out ValeurConvertie))
+                Console.WriteLine($"Conversion réussie ! Valeur convertie : {ValeurConvertie}");
+            else
+                Console.WriteLine("Échec de la conversion.");
+        }
+    }
+}
