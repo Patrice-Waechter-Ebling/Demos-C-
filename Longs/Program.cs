95d524494a146e23966760d0c1486111b5948810
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 13:27:42 -0400

    Ajout nouvelles demos

---
+++ Longs/Program.cs
@@ -0,0 +1,21 @@
+/*********************************************
+		*
+* Projet genere avec CSProjectCreator v:1.01b
+		*
+*  Module:  Longs
+* Developpe par Patrice Waechter-Ebling 2022
+		*********************************************/
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	namespace Longs{	    internal class Program  {
+	        static void Main(string[] args){
+//Votre segment de code  a été inséré ici
+            long myNum = 15000000000L;
+            Console.WriteLine("Long: " + myNum);
+
+		 Console.WriteLine("Appuyez une touche pour quitter");Console.Read(); // informe l'utilisateur de presser une touche pour fermer la console
+		}
+    }
+}
