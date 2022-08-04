fbe626d75fe69c57c25e4f8370add24e5e12affc
Patrice André Waechter-Ebling <97856916+PatriceWaechterEbling@users.noreply.github.com>
2022-08-04 12:35:38 -0400

    Demo utilisatation string[]

---
+++ DateEtHeure2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace DateEtHeure2
+{
+    internal class Program
+    {
+        static public string[] jours = {"Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche" };
+        static public string[] mois = {"janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre" };
+        static void Main(string[] args)
+        {
+            Console.Title = "Bienvenue au cours d AEC";
+            Console.ForegroundColor = ConsoleColor.Cyan | ConsoleColor.DarkGray;
+            Console.WriteLine($"{Console.Title}\nFait le {DateTime.Now} ");
+            Console.WriteLine($"Nous sommes {jours[(int)DateTime.Now.DayOfWeek-1]},le {DateTime.Now.Day} {mois[(int)DateTime.Now.Month - 1]} {DateTime.Now.Year}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
